7f595f2bc2b1c1e716683621c4e84ae16011fbc7
alexbritoizidoro <alexbritoizidoro@gmail.com>
2026-02-25 00:35:53 -0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Clonando código do servidor..."
-git clone https://github.com/alexbritoizidoro/Profissional-tech.git /tmp/code
-cp -r /tmp/code/* /app/
-npm install
-npm run build
-npm start
